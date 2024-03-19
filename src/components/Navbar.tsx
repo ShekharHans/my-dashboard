358610a9d8d817c7433fca3b996630d2d381789f
ShekharHans <shekharhans161@gmail.com>
2024-03-20 00:30:44 +0530

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,20 +3,20 @@ import AppBar from '@mui/material/AppBar';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 
-import Logo from "../assets/react.svg"
+import Logo from "../assets/logo.png"
 
 
 export default function Navbar() {
     return (
         <Stack spacing={2} sx={{ flexGrow: 1 }}>
 
-            <AppBar position="static" color="primary" enableColorOnDark>
+            <AppBar position="static" className='bg-indigo-500' sx={{ '&.MuiAppBar-root': { backgroundColor: '#525f65 !important' } }} enableColorOnDark>
                 <Toolbar>
 
                     <div>
                         <a href="/" className='flex gap-4'>
-                            <img src={Logo} alt="" />
-                            <h1 className='hidden font-heading text-2xl font-semibold sm:inline-block'>Your Company</h1>
+                            <img src={Logo} alt="" width={30} height={30} />
+                            <h1 className='hidden font-heading text-2xl font-semibold sm:inline-block'>AANWIK TECHNOLOGIES</h1>
                         </a>
                     </div>
                 </Toolbar>
